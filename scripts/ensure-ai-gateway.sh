3776dbb368dd7a22b37c034cf4587e6786a7f3c8
qsliu <qsliu2017@outlook.com>
2026-03-23 15:43:16 +0800

    fix ensure-ai-gateway

--- scripts/ensure-ai-gateway.sh
+++ scripts/ensure-ai-gateway.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ -z "$ANTHROPIC_BASE_URL" ] || [ -z "$ANTHROPIC_CUSTOM_HEADERS" ]; then
-  echo "Error: ANTHROPIC_BASE_URL and ANTHROPIC_CUSTOM_HEADERS must be set"
+  echo "Error: ANTHROPIC_BASE_URL and ANTHROPIC_CUSTOM_HEADERS must be set" >&2
+  # TODO: should exit 2 accrodding to docs, but it doesn't work
   exit 1
 fi
+
+exit 0
 
